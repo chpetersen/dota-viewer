3e2deb492d40fe590f286732c1a76d33d58b96be
Christian Flink Petersen <cfp@lunar.app>
2021-12-02 09:33:32 +0100

    Added comment to application class

--- app/src/main/java/dk/cpe/app/DotaViewerApp.kt
+++ app/src/main/java/dk/cpe/app/DotaViewerApp.kt
@@ -3,6 +3,9 @@ package dk.cpe.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Application class for the Dota viewer app
+ */
 @HiltAndroidApp
 class DotaViewerApp: Application() {
 
